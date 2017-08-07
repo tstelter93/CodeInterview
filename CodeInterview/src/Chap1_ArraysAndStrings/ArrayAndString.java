4c6659d33acf7549d1596d557a7084e05885fbae
Timothy Stelter <tstelter@vt.edu>
2017-08-07 18:03:05 -0400

    - Initial Commit

---
+++ CodeInterview/src/Chap1_ArraysAndStrings/ArrayAndString.java
@@ -0,0 +1,10 @@
+package Chap1_ArraysAndStrings;
+
+public class ArrayAndString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Array and Strings");
+	}
+
+}
---
+++ CodeInterview/src/Chap2_LinkedLists/LinkedLists.java
@@ -0,0 +1,10 @@
+package Chap2_LinkedLists;
+
+public class LinkedLists {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("LinkedLists");
+	}
+
+}
